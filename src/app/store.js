8b561a4ae15f133ef070d7fecf768b9c7214c663
zeeeing <zjchew@singnet.com.sg>
2024-12-30 13:03:07 +0900

    configured topics slice

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import reducers
+import topicsReducer from '../features/topics/topicsSlice'
+
+export default configureStore({
+  reducer: {
+    topics: topicsReducer
+  },
+});
---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  topics: {},
+};
+
+const topicsSlice = createSlice({
+  name: "topics",
+  initialState,
+  reducers: {
+    addTopic: (state, action) => {
+      const { id, name, icon } = action.payload;
+      state.topics.topics[id] = {
+        id,
+        name,
+        icon,
+        quizIds: [],
+      };
+    },
+  },
+});
+
+export const selectTopics = (state) => state.topics.topics;
+export const { addTopic } = topicsSlice.actions;
+export default topicsSlice.reducer;
